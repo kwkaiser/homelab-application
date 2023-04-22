b041b9d022349a777bae4be02743d1dafd2b5b2d
Karl Kaiser <karl@kwkaiser.io>
2023-04-22 13:29:28 -0400

    Secret delivery

--- scripts/secrets.sh
+++ scripts/secrets.sh
@@ -1,13 +1,18 @@
 #! /bin/bash
-
 # Sources secrets from a secrets file (testing) or env (prod) & applies to
-# secrets.yaml file
 
 set -o allexport
-
 DIR=$(dirname "$0")
-cd $DIR/..
+cd "$DIR"/.. || exit
 
+# TODO: only apply this in dev test -- not prod
 source ./vars/devtest-secrets.txt
 
-cat ./non-templates/secrets.yaml | envsubst | kubectl apply -f -
+while IFS='=' read -r -d '' n v; do
+  if echo "$n" | grep -i 'k8s_' -q
+  then
+    cleaned_name=$(echo "$n" | cut -c5- | sed 's/_/./g' )
+    kubectl delete secret "$cleaned_name" --ignore-not-found
+    kubectl create secret generic "$cleaned_name" --from-literal=value=bingus
+  fi
+done < <(env -0)
--- templates/helpers.tpl
+++ templates/helpers.tpl
@@ -1,5 +1,5 @@
 {{- define "waitFor" -}}
   - name: {{ .Name }}-init
           image: busybox
-          command: ['sh', '-c', 'until nslookup {{ .Name }}.default.svc.cluster.local; do echo waiting for {{ .Name }}; sleep 2; done;']
+          command: ['sh', '-c', 'until nslookup {{ .Name }}.default.svc.cluster.local; do echo waiting for {{ .Name }}; sleep 5; done;']
 {{- end -}}
