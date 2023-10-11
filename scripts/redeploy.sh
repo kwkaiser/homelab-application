c7dce21cc98a747b27ddd4ee69730f725987b38a
Karl Kaiser <karl@kwkaiser.io>
2023-10-10 22:31:09 -0400

    Use new IPs now that I switched to libvirt locally

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -5,6 +5,6 @@ DIR=$(dirname "$0")
 helm uninstall bingus
 sleep 10 
 helm install bingus .
-cd $HOME/documents/homelab-system
-sleep 10
-make k8s
+# cd "$HOME"/documents/homelab-system || exit
+# sleep 10
+# make k8s
