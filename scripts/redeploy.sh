e0d8e14fe9cf8bad2065db39eef00957340c0d52
Karl Kaiser <karl@kwkaiser.io>
2023-10-26 16:37:34 -0400

    Redeploy

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -1,10 +1,3 @@
 #! /bin/bash
 
-DIR=$(dirname "$0")
-
-helm uninstall bingus
-sleep 10 
-helm install bingus .
-# cd "$HOME"/documents/homelab-system || exit
-# sleep 10
-# make k8s
+helm upgrade bingus . --recreate-pods && echo 'Upgraded!' || helm install bingus .
