0fd9762c6f37ff3609ad32ecc51e63e272bf4084
Karl Kaiser <karl@kwkaiser.io>
2024-02-06 22:28:15 -0500

    Spread values if applicable

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -5,4 +5,4 @@ if [[ $PROD == true ]]; then
   ARGS="--values ./values.yaml --values ./prod.yaml"
 fi
 
-helm upgrade bingus . "$ARGS" --dependency-update && echo 'Updated!' || helm install bingus . "$ARGS" --dependency-update
+helm upgrade bingus . $ARGS --dependency-update && echo 'Updated!' || helm install bingus . $ARGS --dependency-update
