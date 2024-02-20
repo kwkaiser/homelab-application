73768f82e6181f6831217ef9b8f3ae4ef50dee50
Karl Kaiser <karl@kwkaiser.io>
2024-02-19 19:17:08 -0500

    Make it easier to load values with respect to env

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -2,7 +2,9 @@
 
 ARGS=""
 if [[ $PROD == true ]]; then
-  ARGS="--values ./values.yaml --values ./prod.yaml"
+  ARGS="--values ./values/shared.yaml --values ./values/prod.yaml"
+else
+  ARGS="--values ./values/shared.yaml --values ./values/dev.yaml"
 fi
 
 helm upgrade bingus . $ARGS --dependency-update && echo 'Updated!' || helm install bingus . $ARGS --dependency-update
