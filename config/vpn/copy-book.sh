6d0a038f406e98bb68d359f13973287ab773398c
Karl Kaiser <karl@kwkaiser.io>
2024-05-15 20:37:44 -0400

    rm file quotes

--- config/vpn/copy-book.sh
+++ config/vpn/copy-book.sh
@@ -3,4 +3,4 @@
 echo "Copying books from torrent: '$1'"
 
 FILES=$(find "$1" -type f -regex '.*\.\(pdf\|mobi\|epub\|azw\|azw3\|djvu\|fb2\|lit\|pdb\)$' | grep -i '/shared/torrents/books' | xargs)
-cp "$FILES" /shared/upload/
+cp $FILES /shared/upload/
