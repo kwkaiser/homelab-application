51185d2281be72948c61dec6af6c3e3aea57723b
Karl Kaiser <karl@kwkaiser.io>
2024-05-15 21:41:20 -0400

    Swap for kasm firefox

--- config/vpn/copy-book.sh
+++ config/vpn/copy-book.sh
@@ -9,6 +9,13 @@ fi
 
 echo "Copying books from torrent: '$1'"
 
-cd "$1"
+# If it's a file, just copy the file
+if [[ -f "$1" ]];
+then
+  cp "$1" /shared/upload
+  exit 0
+fi
 
+# If it's a dir, copy each file within the dir
+cd "$1"
 find . -type f -regex '.*\.\(pdf\|mobi\|epub\|azw\|azw3\|djvu\|fb2\|lit\|pdb\)$' -exec cp {} /shared/upload \;
