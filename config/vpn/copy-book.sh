a85127980e2ef9013b5e6d57a4191c6ef883ad8c
Karl Kaiser <karl@kwkaiser.io>
2024-05-15 20:33:57 -0400

    Use xargs

--- config/vpn/copy-book.sh
+++ config/vpn/copy-book.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-FILES=$(find "$1" -type f -regex '.*\.\(pdf\|mobi\|epub\|azw\|azw3\|djvu\|fb2\|lit\|pdb\)$' | grep -i '/shared/torrents/books')
+echo "Copying books from torrent: '$1'"
+
+FILES=$(find "$1" -type f -regex '.*\.\(pdf\|mobi\|epub\|azw\|azw3\|djvu\|fb2\|lit\|pdb\)$' | grep -i '/shared/torrents/books' | xargs)
 cp "$FILES" /shared/upload/
